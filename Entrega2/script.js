da13152a3f4d918485edacd4bd37bcdc31e188a5
axel <axeljonta@gmail.com>
2022-05-19 18:04:44 -0300

    l

--- Entrega2/script.js
+++ Entrega2/script.js
@@ -1,13 +1,3 @@
-// save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
-// getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
-// getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
-// deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
-// deleteAll(): void - Elimina todos los objetos presentes en el archivo.
-// El método save incorporará al producto un id numérico, que deberá ser siempre uno más que el id del último objeto agregado (o id 1 si es el primer objeto que se agrega) y no puede estar repetido.
-// Tomar en consideración el contenido previo del archivo, en caso de utilizar uno existente.
-// Implementar el manejo de archivos con el módulo fs de node.js, utilizando promesas con async/await y manejo de errores.
-// Probar el módulo creando un contenedor de productos, que se guarde en el archivo: “productos.txt”
-// Incluir un llamado de prueba a cada método, y mostrando por pantalla según corresponda para verificar el correcto funcionamiento del módulo construído. 
 
 const fs = require('fs')
 
@@ -89,7 +79,8 @@ const gestor = new Contenedor('archivo.txt');
     //await gestor.getAll();
     await gestor.deleteById(2);
     await gestor.getAll(); 
-    //await gestor.deleteAll() este si funciona
+    //await gestor.deleteAll() 
+    //
     
 })();
 
